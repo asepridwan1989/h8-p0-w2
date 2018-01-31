c9413e612b11b3d97a5e8d9955da2b4e1367c48f
asepridwan1989 <aseprmail@gmail.com>
2018-02-01 02:33:46 +0700

    upload tugas 5

---
+++ exercise-5.js
@@ -0,0 +1,93 @@
+//No. 1 Let.s Form a Sentence-----------------------------------------------------------------------------------------
+
+console.log('No. 1 Let.s Form a Sentence');
+var word = 'JavaScript';
+var second = 'is';
+var third = 'awesome';
+var fourth = 'and';
+var fifth = 'I';
+var sixth = 'love';
+var seventh = 'it!';
+
+ //output : JavaScript is awesome and I love it!
+
+ console.log(word + ' ' + second + ' ' + third + ' ' + fourth + ' ' + fifth + ' ' + sixth + ' ' + seventh);
+ console.log('_________________________________________________________');
+//No.2 Index Accessing -1 by 1-----------------------------------------------------------------------------------------
+console.log('No.2 Index Accessing -1 by 1');
+var word = 'wow JavaScript is so cool';
+var exampleFirstWord = word[0] + word[1] + word[2];
+var secondWord = word[4] + word[5] + word[6] + word[7] + word[8] + word[9] + word[10] + word[11] + word[12] + word[13]; // do your own!
+var thirdWord = word[15] + word[16]; // do your own!
+var fourthWord = word[18] + word[19]; // do your own!
+var fifthWord = word[21] + word[22] + word[23] + word[24]; // do your own!
+
+/*
+output:
+First Word: wow
+Second Word: JavaScript
+Third Word: is
+Fourth Word: so
+Fifth Word: cool
+*/
+
+console.log('First Word: ' + exampleFirstWord);
+console.log('Second Word: ' + secondWord);
+console.log('Third Word: ' + thirdWord);
+console.log('Fourth Word: ' + fourthWord);
+console.log('Fifth Word: ' + fifthWord);
+ console.log('_________________________________________________________');
+//No. 3 Breaking Sentence (Again) using Substring-------------------------------------------------------------------
+console.log('No. 3 Breaking Sentence (Again) using Substring');
+var word3 = 'wow JavaScript is so cool';
+var exampleFirstWord3 = word.substring(0, 3);
+var secondWord3 = word.substring(4, 14); // do your own!
+var thirdWord3 = word.substring(15, 17);// do your own!
+var fourthWord3 = word.substring(18, 20); // do your own!
+var fifthWord3 = word.substring(21, 25); // do your own!
+
+/*
+output:
+First Word: wow
+Second Word: JavaScript
+Third Word: is
+Fourth Word: so
+Fifth Word: cool
+*/
+
+console.log('First Word: ' + exampleFirstWord3);
+console.log('Second Word: ' + secondWord3);
+console.log('Third Word: ' + thirdWord3);
+console.log('Fourth Word: ' + fourthWord3);
+console.log('Fifth Word: ' + fifthWord3);
+console.log('_________________________________________________________');
+// No. 4 Breaking Sentence (yet Again) and Count Each Length-----------------------------------------------------------
+console.log('No. 4 Breaking Sentence (yet Again) and Count Each Length');
+var word4 = 'wow JavaScript is so cool';
+var exampleFirstWord4 = word.substring(0, 3);
+var secondWord4 = word.substring(4, 14); // do your own!
+var thirdWord4 = word.substring(15, 17); // do your own!
+var fourthWord4 = word.substring(18, 20); // do your own!
+var fifthWord4 = word.substring(21, 25); // do your own!
+
+
+var firstWordLength = exampleFirstWord4.length;
+var SecondWordLength = secondWord4.length;
+var ThirdWordLength = thirdWord4.length;
+var fourthWordLength = fourthWord4.length;
+var FifthWordLength = fifthWord4.length;
+
+/*
+output:
+First Word: wow, with length: 3
+Second Word: JavaScript, with length: 10
+Third Word: is, with length: 2
+Fourth Word: so, with length: 2
+Fifth Word: cool, with length: 4
+*/
+
+console.log('First Word: ' + exampleFirstWord4 + ', with length: ' + firstWordLength);
+console.log('Second Word: ' + secondWord4 + ', with length: ' + SecondWordLength);
+console.log('Third Word: ' + thirdWord4 + ', with length: ' + ThirdWordLength);
+console.log('Fourth Word: ' + fourthWord4 + ', with length: ' + fourthWordLength);
+console.log('Fifth Word: ' + fifthWord4 + ', with length: ' + FifthWordLength);
