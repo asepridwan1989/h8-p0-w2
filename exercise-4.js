907bfd973f440c8833b365e05a2ed0b9f2f90712
asepridwan1989 <aseprmail@gmail.com>
2018-02-02 14:33:29 +0700

    revisi 3

--- exercise-4.js
+++ exercise-4.js
@@ -1,3 +1,4 @@
+//-------------------------------tugas switch case-----------------------------------------------
 var tanggal = 21; // assign nilai variabel tanggal disini! (dengan angka antara 1 - 31)
 var bulan = 1; // assign nilai variabel bulan disini! (dengan angka antara 1 - 12)
 var tahun = 1945; // assign nilai variabel tahun disini! (dengan angka antara 1900 - 2200)
